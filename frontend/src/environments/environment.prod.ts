dae3460c48cd9d56a14813032aa393a79bc6bc20
dorival <dorival.querino@gmail.com>
2019-06-05 21:34:34 -0300

    Firs news api

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey:'36592793f5bd40128497c97570bf22bf',
+  apiUrl: 'https://newsapi.org/v2'
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey:'36592793f5bd40128497c97570bf22bf',
+  apiUrl: 'https://newsapi.org/v2'
 };
 
 /*
